67b761f18b946b5ddf58e02f28f83e068c73a01e
Dawid Haratyk <dawidharatyk@onet.pl>
2024-09-15 23:03:14 +0200

    fix: pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/katowice-conf-web/",
+  base: "",
   plugins: [react()],
   resolve: {
     alias: {
